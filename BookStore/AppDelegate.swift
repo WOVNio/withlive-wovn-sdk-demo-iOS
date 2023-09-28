5aa9389fa11d3e4e898d403377242c9abf7d66e2
Anh Vu <danganhvu1998@gmail.com>
2023-09-28 15:36:55 +0900

    Second Commit: Enable EventReporting; Set permission; Change language.

--- BookStore/AppDelegate.swift
+++ BookStore/AppDelegate.swift
@@ -20,6 +20,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             BookStoreConfiguration.shared.setBaseURL(URL(string: "http://localhost:8080")!)
         }
         print("WOVN SDK VERSION: \(Wovn.sdkVersion)")
+        // Enable Wovn EventReporting feature
+        Wovn.eventTypeReport()
+        // Test allow report data. Do not hard code this in production
+        Wovn.setPermission(permission: WovnPermission.all, enabled: true)
+        // Change language
+        Wovn.changeLang(langs: "ja", callback: { langResult in
+            print("WOVN SDK ChangLang Result: \(langResult)")
+        })
         return true
     }
 }
